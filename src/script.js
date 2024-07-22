f0b0dd6b03a7d9a663d82107e97b8c24be7d3579
Matt-Kostka <matthew.kostka08@gmail.com>
2024-07-22 02:10:10 +0100

    finished

--- src/script.js
+++ src/script.js
@@ -17,14 +17,46 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
-scene.add(ambientLight)
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
+// scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 50)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+// // Directional light
+// const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9)
+// scene.add(directionalLight)
+
+// // Hemisphere light
+// const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.9)
+// scene.add(hemisphereLight)
+
+// const pointLight = new THREE.PointLight(0xffffff, 50)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+// scene.add(pointLight)
+
+// // React area light
+// const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 6, 1, 1)
+// rectAreaLight.position.set(- 1.5, 0, 1.5)
+// rectAreaLight.lookAt(new THREE.Vector3())
+// scene.add(rectAreaLight)
+
+// // Spot light
+// const spotLight = new THREE.SpotLight(0x78ff00, 4.5, 10, Math.PI * 0.1, 0.25, 1)
+// spotLight.position.set(0, 2, 3)
+// scene.add(spotLight)
+
+// // Helpers
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+// scene.add(hemisphereLightHelper)
+
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+// scene.add(directionalLightHelper)
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+// scene.add(pointLightHelper)
+
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+// scene.add(spotLightHelper)
 
 /**
  * Objects
